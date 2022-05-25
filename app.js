c95683fd3c18e15018d617d771a09166803d4ea7
michaelc42 <michaelcasey42@gmail.com>
2022-05-24 23:03:40 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// GLOBALS - NO WINDOW!!!!
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - infor about current module (file) all files are modules.  need to export any tools or data to use in other files
+// process - info about env where the program is being executed
+
+const _ = require('lodash')
